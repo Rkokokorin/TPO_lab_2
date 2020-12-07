59d9328a2edaab42f28b9cda214d918dd7ab2f7d
Roman Kokorin <k.r.007.015@gmail.com>
2020-12-07 12:08:35 +0300

    TrigonometricTests

--- src/Logic/TrigonometricFunctions.java
+++ src/Logic/TrigonometricFunctions.java
@@ -5,24 +5,23 @@ public class TrigonometricFunctions {
     private static final int n = 8;
 
     public  double sin(double x) {
-        x = normalizeX(x);
-        int sign = ((x > -Math.PI / 2) && (x < Math.PI / 2)) ? 1 : -1;
+        double x1 = normalizeX(x);
+        int sign = ((x1 > 0) && (x1 < Math.PI )) ? 1 : -1;
         return sign * Math.pow(1 - Math.pow(cosFunction.cos(x), 2), 0.5);
+    }
+    public double cos(double x) {
+        return cosFunction.cos(x) ;
     }
     public double tan(double x) {
         return sin(x) / cosFunction.cos(x);
     }
-    public double cot(double x) {
-        return cosFunction.cos(x) / sin(x);
-    }
-
+    public double cot(double x) { return cosFunction.cos(x) / sin(x); }
     public double csc(double x) {
         return 1 / sin(x);
     }
     public double sec(double x) {
         return 1 / cosFunction.cos(x);
     }
-
     private double normalizeX(double x) {
         return x % (2 * Math.PI);
     }
--- src/Test/TrigonometricTest.java
+++ src/Test/TrigonometricTest.java
@@ -1,8 +1,12 @@
 package Test;
 
+import Logic.CosFunction;
 import Logic.LogarithmicFunctions;
 import Logic.TrigonometricFunctions;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
@@ -10,31 +14,60 @@ import static org.junit.jupiter.api.Assertions.*;
 import static java.lang.Math.*;
 import static org.mockito.Mockito.*;
 
-
-
 public class TrigonometricTest {
-        static final double EPS = 0.0001;
-        static final double DELTA = 0.001;
+        static final double DELTA = 0.01;
+        static CosFunction cosMock;
         static  TrigonometricFunctions trigonometricFunctions ;
 
-//        @BeforeAll
-//        static void setup() {
-//            sinMock = mock(SinDecomposer.class);
-//            when(sinMock.calculate(0.0, EPSILON)).thenReturn(0.0);
-//            when(sinMock.calculate(PI / 6, EPSILON)).thenReturn(0.5);
-//            when(sinMock.calculate(PI / 4, EPSILON)).thenReturn(sqrt(2) / 2);
-//            when(sinMock.calculate(PI / 3, EPSILON)).thenReturn(sqrt(3) / 2);
-//            when(sinMock.calculate(PI / 2, EPSILON)).thenReturn(1.0);
-//            when(sinMock.calculate(PI, EPSILON)).thenReturn(0.0);
-//            when(sinMock.calculate(PI * 5 / 4, EPSILON)).thenReturn(-sqrt(2) / 2);
-//            when(sinMock.calculate(PI * 3 / 2, EPSILON)).thenReturn(-1.0);
-//            when(sinMock.calculate(PI / 4 + PI/2, EPSILON)).thenReturn(1/sqrt(2));
-//            when(sinMock.calculate(PI * 7 / 4, EPSILON)).thenReturn(-sqrt(2) / 2);
-//
-//            trigonometryFunctions = new TrigonometryFunctions(sinMock);
-//        }
-//        }
+        @BeforeEach
+      void setup() {
+            cosMock = mock(CosFunction.class);
+            when(cosMock.cos(0.0)).thenReturn(1.);
+            when(cosMock.cos(PI / 6)).thenReturn(sqrt(3) / 2);
+            when(cosMock.cos(PI / 4)).thenReturn(sqrt(2) / 2);
+            when(cosMock.cos(PI / 3)).thenReturn(0.5);
+            when(cosMock.cos(PI / 2)).thenReturn(0.);
+            when(cosMock.cos(PI)).thenReturn(-1.);
+            when(cosMock.cos(PI * 5 / 4)).thenReturn(-sqrt(2) / 2);
+            when(cosMock.cos(PI * 3 / 2)).thenReturn(0.);
+            when(cosMock.cos(3*PI / 4)).thenReturn(-sqrt(2) / 2);
+            when(cosMock.cos(PI * 7 / 4)).thenReturn(-sqrt(2) / 2);
+            trigonometricFunctions = new TrigonometricFunctions();
+        }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles =  {0, PI/2, PI/4, PI*3/4, PI, PI*5/4})
+    void SinTest(double x) {
+        assertEquals(Math.sin(x), trigonometricFunctions.sin(x), DELTA);
+    }
+    @ParameterizedTest
+    @ValueSource(doubles =  {0, PI/4, PI*3/4, PI, PI*5/4})
+    void secTest(double x) {
+        assertEquals(1 / Math.cos(x), trigonometricFunctions.sec(x), DELTA);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles =  {0, PI/4, PI*3/4, PI, PI*5/4})
+    void tanTest(double x) {
+        assertEquals(Math.sin(x) / Math.cos(x), trigonometricFunctions.tan(x), DELTA);
+    }
+    @ParameterizedTest
+    @ValueSource(doubles =  { PI/4, PI*3/4,PI/6, PI/3, PI*5/4})
+    void cotTest(double x) {
+        assertEquals(Math.cos(x)/Math.sin(x) , trigonometricFunctions.cot(x), DELTA);
+    }
+    @ParameterizedTest
+    @ValueSource(doubles =  { PI/6, PI/4, PI/3, PI/2,3*PI/2})
+    void cscTest(double x) {
+        assertEquals(1/sin(x) , trigonometricFunctions.csc(x), DELTA);
+    }
 
 
 
-}
+
+        }
+
+
+
+
